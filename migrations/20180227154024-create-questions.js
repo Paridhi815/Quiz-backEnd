181d0764ea82e15265eb960cf67326d43c066000
paridhi <paridhi815@gmail.com>
2018-02-27 23:25:46 +0530

    Made models and migrations-question and option table

---
+++ migrations/20180227154024-create-questions.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('questions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      question: {
+        type: Sequelize.STRING
+      },
+      qid: {
+        type: Sequelize.INTEGER
+      },
+      correctAns: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('questions');
+  }
+};
