b89ace29772ab9934d71eef1172d56a864548424
paridhi <paridhi815@gmail.com>
2018-02-28 00:41:36 +0530

    GET /readDb fetching data from the dataBase done

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 // const ratings = require('./ratings');
 const getQuesAndStoreInDb = require('./getQuesAndStoreInDb');
+const storeOptions = require('./getOptions');
+const readDb = require('./readDb');
 
-
-module.exports = [].concat(getQuesAndStoreInDb);
+module.exports = [].concat(getQuesAndStoreInDb, storeOptions, readDb);
---
+++ src/routes/readDb.js
@@ -0,0 +1,43 @@
+// const rp = require('request-promise');
+const Models = require('../../models');
+
+const handler = (request, response) => {
+  Models.questions.findAll({ raw: true }).then((result) => {
+    // console.log('-----', result);
+    const optionsPromises = [];
+    const questionsWithOptions = [];
+    result.forEach((question) => {
+      const options = [];
+      optionsPromises.push(Models.options.findAll({
+        where: {
+          qid: question.qid,
+        },
+        raw: true,
+      }).then((optionArray) => {
+        // console.log('>>>>>>>>>>', optionArray);
+        optionArray.forEach((optionObject) => {
+          options.push(optionObject.option);
+        });
+        question.options = options;
+        questionsWithOptions.push(question);
+        // console.log('pari', questionsWithOptions);
+      }));
+    });
+    Promise.all(optionsPromises).then(() => {
+      console.log(questionsWithOptions);
+      response(questionsWithOptions);
+    });
+
+
+    // Promise.all(optionsPromises).then(() => {
+    //   response();
+    // });
+  });
+};
+
+
+module.exports = {
+  method: 'GET',
+  path: '/readDb',
+  handler,
+};
