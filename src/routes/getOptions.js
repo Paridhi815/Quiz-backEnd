19db5316ec94ad5b9476a3434e59b6cf13b5870b
paridhi <paridhi815@gmail.com>
2018-02-28 00:47:14 +0530

    Refactored the code

--- src/routes/getOptions.js
+++ src/routes/getOptions.js
@@ -8,16 +8,6 @@ const handler = (request, response) => {
   rp(api1).then((data) => {
     // console.log(data);
     const allTheQuestions = JSON.parse(data);
-    // const arr = [];
-    // for (let i = 0; i < allTheQuestions.allQuestions.length; i += 1) {
-    //   const correctAnswersPromise = rp(`${api2}${allTheQuestions.allQuestions[i].questionId}`);
-    //   arr.push(correctAnswersPromise);
-    // }
-    // Promise.all(arr).then((data1) => {
-    //   for (let i = 0; i < data1.length; i += 1) {
-    //     const ans = JSON.parse(data1[i]);
-    //     allTheQuestions.allQuestions[i].correctAnswer = ans.answer;
-    //   }
     allTheQuestions.allQuestions.forEach((question) => {
       const questionKeys = Object.keys(question);
       questionKeys.forEach((key) => {
@@ -31,17 +21,7 @@ const handler = (request, response) => {
       });
     });
 
-    //   const allOptionsPerQuestion = allTheQuestions.allQuestions.map(question => ({
-    //     qid: question.questionId,
-    //     correctAns: question.correctAnswer,
-    //   }));
-    //   console.log(allOptionsPerQuestion);
-    //   Models.options.destroy({ truncate: true });
-    //   Models.options.bulkCreate(allOptionsPerQuestion).then((data2) => {
-    //     response(data2);
-    //   });
     response('allQuestionsWithCorrectAnswers');
-    // });
   });
 };
 
